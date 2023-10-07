5fc1dd8857bc1d65b79be080fefc13c37c432810
CGQAQ <m.jason.liu@outlook.com>
2023-10-07 17:19:05 +0800

    comment

--- src/lib.js
+++ src/lib.js
@@ -1,31 +1,28 @@
 const bindings = require("bindings")("my_addon.node");
 
 (async () => {
-
+	// start
+	console.log("\n\n");
 	console.log("-----------------------------------------------");
 	console.log("------------test throw from C++----------------");
 	try {
-	  bindings.crash();
+		bindings.crash();
 	} catch (e) {
-	  console.log(e);
+		console.log(e);
 	}
 	console.log("-----------------------------------------------\n\n");
-	
+
 	console.log("-----------------------------------------------");
 	console.log("-----------test add number from C++------------");
 	const addResult = bindings.add(1, 2, 3, 4);
 	console.log(`add 1,2,3,4 from C++, the result: ${addResult}`);
 	console.log("-----------------------------------------------\n\n");
-	
-	
+
 	console.log("-----------------------------------------------");
-	
+
 	console.log("---------test wrapInPromise from C++-----------");
 	const wrapInPromiseResult = await bindings.wrapInPromise("Hello world!");
 	console.log("wrapInPromise from C++, the result: ", wrapInPromiseResult);
 	console.log("-----------------------------------------------\n\n");
-
+	// end
 })();
-
-
-
