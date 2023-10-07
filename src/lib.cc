5e863175addd32f38d3f31b48940e01388988dbb
CGQAQ <m.jason.liu@outlook.com>
2023-10-07 16:14:06 +0800

    add from c++

--- src/lib.cc
+++ src/lib.cc
@@ -1,33 +1,94 @@
 #include <node_api.h>
+#include <iostream>
 
 #include "lib.h"
 
-static napi_value crash(napi_env env, napi_callback_info info) {
-    napi_throw_error(env, NULL, "Throwing an error from C++, hooray!!");
-    return nullptr;
+static napi_value crash(napi_env env, napi_callback_info info)
+{
+	napi_throw_error(env, NULL, "Throwing an error from C++, hooray!!");
+	return nullptr;
 }
 
-napi_value create_addon(napi_env env) {
-  napi_value result;
-  NAPI_CALL(env, napi_create_object(env, &result));
+static napi_value add(napi_env env, napi_callback_info info)
+{
+	constexpr size_t kArgCount{10};
+	size_t argc{kArgCount};
+	napi_value argv[kArgCount];
+	napi_value this_arg{nullptr};
+	void *data{nullptr};
+	NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, &this_arg, &data));
 
-  napi_value exported_function;
-  NAPI_CALL(env, napi_create_function(env,
-                                      "crash",
-                                      NAPI_AUTO_LENGTH,
-                                      crash,
-                                      nullptr,
-                                      &exported_function));
+	if (argc < 2)
+	{
+		napi_throw_error(env, NULL, "Wrong number of arguments, expected >= 2");
+		return nullptr;
+	}
 
-  NAPI_CALL(env, napi_set_named_property(env,
-                                         result,
-                                         "crash",
-                                         exported_function));
+	int32_t acc{0};
+	for (size_t i{0}; i < kArgCount; ++i)
+	{
+		napi_handle_scope scope{};
+		napi_open_handle_scope(env, &scope);
 
-  return result;
-} 
+		napi_valuetype valuetype;
+		NAPI_CALL(env, napi_typeof(env, argv[i], &valuetype));
+		if (valuetype == napi_number)
+		{
+			int32_t value{0};
+			NAPI_CALL(env, napi_get_value_int32(env, argv[i], &value));
+			acc += value;
+		}
+		else if (valuetype == napi_undefined)
+		{
+			// do nothing
+		}
+		else if (valuetype != napi_number)
+		{
+			napi_throw_error(env, NULL, "Wrong argument type, expected number");
+			return nullptr;
+		}
 
+		napi_close_handle_scope(env, scope);
+	}
 
-NAPI_MODULE_INIT() {
-    return create_addon(env);
+	napi_value result{};
+	NAPI_CALL(env, napi_create_int32(env, acc, &result));
+
+	return result;
+}
+
+napi_value create_addon(napi_env env)
+{
+	napi_value result;
+	NAPI_CALL(env, napi_create_object(env, &result));
+
+	napi_value fn_crash;
+	NAPI_CALL(env, napi_create_function(env,
+										"crash",
+										NAPI_AUTO_LENGTH,
+										crash,
+										nullptr,
+										&fn_crash));
+	NAPI_CALL(env, napi_set_named_property(env,
+										   result,
+										   "crash",
+										   fn_crash));
+
+	napi_value fn_add;
+	NAPI_CALL(env, napi_create_function(env,
+										"add",
+										NAPI_AUTO_LENGTH,
+										add,
+										nullptr,
+										&fn_add));
+	NAPI_CALL(env, napi_set_named_property(env,
+										   result,
+										   "add",
+										   fn_add));
+	return result;
+}
+
+NAPI_MODULE_INIT()
+{
+	return create_addon(env);
 }
--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,16 @@
-var bindings = require('bindings')('my_addon.node');
+const bindings = require("bindings")("my_addon.node");
 
+console.log("-----------------------------------------------");
+console.log("------------test throw from C++----------------");
+try {
+  bindings.crash();
+} catch (e) {
+  console.log(e);
+}
+console.log("-----------------------------------------------\n\n");
 
-bindings.crash();
+console.log("-----------------------------------------------");
+console.log("-----------test add number from C++------------");
+const addResult = bindings.add(1, 2, 3, 4);
+console.log(`add 1,2,3,4 from C++, the result: ${addResult}`);
+console.log("-----------------------------------------------\n\n");
